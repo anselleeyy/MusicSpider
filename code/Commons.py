c41dc347e0c5080bf9523bf6398e2aaa7ee4a0a3
anselleeyy <573459407@qq.com>
2018-12-10 18:52:08 +0800

    更新 headers

--- code/Commons.py
+++ code/Commons.py
@@ -1,14 +1,11 @@
 headers = {
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:57.0) Gecko/20100101 Firefox/57.0',
-    'Referer': 'https://music.163.com',
     'Accept': '*/*',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-    'Host': 'music.163.com',
     'Accept-Language': 'zh-CN,en-US;q=0.7,en;q=0.3',
-    'DNT': '1',
-    'Pragma': 'no-cache'
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'music.163.com',
+    'Referer': 'https://music.163.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/70.0.3538.110 Safari/537.36'
 }
 
 dir_root = 'C:/Users/Ansel/Desktop/MusicSpider/music/'
