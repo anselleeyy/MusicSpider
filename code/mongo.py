de0c2345bd14044489a820f6b4e067d3b5d8d576
anselleeyy <573459407@qq.com>
2018-12-11 20:45:05 +0800

    新增歌手、歌曲保存方法，并完善 MongoDB 相关操作的注释

--- code/mongo.py
+++ code/mongo.py
@@ -7,15 +7,48 @@ class Mongo:
         self.port = 27017
         self.client = MongoClient(host=self.host, port=self.port)
         self.db = self.client.music
-        # my_set = db.li
+        self.collections = self.db.album
 
-    def save_album_info(self, data, artist):
-        collections = self.db.album
-        collections.update({"artist": artist}, {"$push": {"albums": data}})
+    # 保存歌手相关信息
+    def save_artist_info(self, artist_info):
+        # 处理歌手信息
+        data = {
+            "name": artist_info['name'],
+            "id": artist_info['id'],
+            "picUrl": artist_info['picUrl']
+        }
+        # 新增该歌手的信息
+        result_id = self.collections.insert({"artist": data})
+        return result_id
+
+    # 保存单张专辑信息
+    def save_album_info(self, album_info, artist):
+        data = {
+            "name": album_info['name'],
+            "id": album_info['id'],
+            "size": album_info['size'],
+            "picUrl": album_info['picUrl'],
+            "publishTime": album_info['publishTime'],
+            "commentThreadId": album_info['commentThreadId'],
+            "description": album_info['description'],
+            "tags": album_info['tags'],
+            "company": album_info['company']
+        }
+        self.collections.update({"artist.name": artist}, {"$push": {"albums": data}})
         return
 
+    # 判断专辑是否存在
     def find_if_exist(self, artist, album):
-        collections = self.db.album
-        result = collections.find({"artist": artist, "albums.name": album})
-        print(result)
+        result = self.collections.find({"artist.name": artist, "albums.name": album})
         return result.count()
+
+    # 保存歌曲信息
+    def save_music_info(self, song_info, album_name, artist, mp3Url, lyric):
+        data = {
+            "name": song_info['name'],
+            "id": song_info['id'],
+            "mp3Url": mp3Url,
+            "lyric": lyric
+        }
+        self.collections.update({"artist.name": artist, "albums.name": album_name}, {"$push": {"albums.$.songs": data}})
+        return
