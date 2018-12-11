66efc583593b45dbfc001ac4c035681d48c029f8
anselleeyy <573459407@qq.com>
2018-12-11 20:00:03 +0800

    更新 Commons 中的变量名，新增专辑最大数 MAX_SIZE

--- code/Commons.py
+++ code/Commons.py
@@ -8,12 +8,14 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/70.0.3538.110 Safari/537.36'
 }
 
-dir_root = 'C:/Users/Ansel/Desktop/MusicSpider/music/'
+DIR_ROOT = '/Users/tangshuning/PycharmProjects/MusicSpider/music/'
 
-search_url = 'https://music.163.com/weapi/cloudsearch/get/web?csrf_token'
+SEARCH_URL = 'https://music.163.com/weapi/cloudsearch/get/web?csrf_token'
 
-album_url = 'http://music.163.com/api/album/{}'
+ALBUM_URL = 'http://music.163.com/api/album/{}'
 
-song_url = 'http://music.163.com/weapi/song/enhance/player/url?csrf_token='
+SONG_URL = 'http://music.163.com/weapi/song/enhance/player/url?csrf_token='
 
-lyric_url = 'https://music.163.com/weapi/song/lyric?csrf_token='
+LYRIC_URL = 'https://music.163.com/weapi/song/lyric?csrf_token='
+
+ALBUM_MAX_SIZE = 30
