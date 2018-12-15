2d5185e85c64540feb40d1a7f01105d9da212c22
anselleeyy <573459407@qq.com>
2018-12-15 19:15:47 +0800

    完善本地文件保存和读取功能

--- code/store.py
+++ code/store.py
@@ -1,50 +1,53 @@
-from code import Commons
-import os, json
+import json
+import os
 from urllib import request
+
+from code import Commons
 
 
 class Store:
     def __init__(self):
-        self.root = Commons.DIR_ROOT
+        self.root = Commons.DOWNLOAD_DIR
         self.opener = request.build_opener()
         self.opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
 
     # 保存歌手所有专辑信息到本地（Json 文件）
-    def save_all_album_info(self, artist, info):
+    def save_all_album_info(self, _id, artist, info):
         # 目录地址
-        dir_path = self.root + '{}/'.format(artist)
+        dir_path = self.root + '{}/'.format(_id)
         # 文件夹不存在，则创建
         if not os.path.exists(dir_path):
             os.mkdir(dir_path)
-        path = dir_path + '{}.json'.format(artist)
-        file = open(path, 'w', encoding='utf-8')
+        file_path = dir_path + '{}.json'.format(artist)
+        file = open(file_path, 'w', encoding='utf-8')
         json.dump(info, file, ensure_ascii=False, indent=4)
         print('---------- artist: %s albums info saved ----------' % artist)
         file.close()
-        return path
+        return file_path
 
     # 保存单个专辑信息到本地（Json 文件）
-    def save_album_info(self, artist, album_name, info):
+    def save_album_info(self, _id, album_id, artist, album_name, info):
         # 目录地址
-        dir_path = self.root + '{}/{}/'.format(artist, album_name)
+        dir_path = self.root + '{}/{}/'.format(_id, album_id)
         # 文件夹不存在，则创建
         if not os.path.exists(dir_path):
             os.mkdir(dir_path)
-        path = dir_path + '{}.json'.format(album_name)
-        file = open(path, 'w', encoding='utf-8')
+        file_path = dir_path + '{}.json'.format(album_name)
+        file = open(file_path, 'w', encoding='utf-8')
         json.dump(info, file, ensure_ascii=False, indent=4)
         print('---------- artist: %s \'s album: %s info saved ----------' % (artist, album_name))
         file.close()
-        return path
+        return file_path
 
     # 下载工具（包括音乐、图片）
     def download(self, url, path):
+        dir_path = self.root + path
         request.install_opener(self.opener)
         file_name = url.split('/').pop()
         # 判断目录是否存在，如果不存在，则创建
-        if not os.path.exists(path):
-            os.makedirs(path)
-        file_path = os.path.join(path, file_name)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        file_path = os.path.join(dir_path, file_name)
         print(file_path)
         request.urlretrieve(url=url, filename=file_path)
         return file_path
