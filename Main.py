46d102c0fa43661169b141d1162ba353289460b5
unknown <liyaoyu@wenxue.root>
2019-01-09 13:44:14 +0800

    update version 2

--- Main.py
+++ Main.py
@@ -9,33 +9,29 @@ if __name__ == '__main__':
     param = Commons.artists
     api = Api()
     store = Store()
+    # 2019.1.9 更新: 更换 MongoDB 存储格式
     mongo = Mongo()
     # 遍历歌手参数
     for artist in param:
         # 得到当前歌手信息并保存
         artist_info = api.get_artist_info(artist=artist)
-        _id = mongo.save_artist_info(artist_info=artist_info['result']['artists'][0])
-        print(_id)
+        _id = mongo.save_artist_info(artist_info = artist_info['result']['artists'][0])
+        print("artist_id: {}".format(_id))
         # 获取歌手所有专辑信息并保存
         album_info = api.get_artist_album_list(artist=artist)
         album_info_path = store.save_all_album_info(_id=_id, artist=artist, info=album_info)
         # 遍历专辑信息
         albums = store.read_file(path=album_info_path)
         # 实际专辑数量
-        count = 0
         album_count = albums['result']['albumCount']
         # 每个歌手的专辑数量不得超过MAX_SIZE（这里我们选择30）
         # 同时判断当前歌手的专辑数量是否超过30
         album_count = album_count if (album_count < Commons.ALBUM_MAX_SIZE) else Commons.ALBUM_MAX_SIZE
         for i in range(album_count):
             item = albums['result']['albums'][i]
-            # 避免同名专辑存在（尚没有解决同歌手的同名专辑问题）
-            if mongo.find_if_exist(artist=artist, album=item['name']) != 0:
-                continue
             # 下载专辑图片
             picUrl = store.download(url=item['picUrl'], path=os.path.join(str(_id), str(item['id'])))
-            mongo.save_album_info(album_info=item, artist=artist, pic_url=picUrl)
-            count += 1
+            mongo.save_album_info(album_info=item, artist_id=_id, pic_url=picUrl)
             # 获取当前专辑内音乐信息
             music_info = api.get_album_info(album_id=item['id'])
             music_info_path = store.save_album_info(_id=_id, album_id=item['id'], artist=artist, album_name=item['name'], info=music_info)
@@ -48,7 +44,5 @@ if __name__ == '__main__':
                     continue
                 path = store.download(url=mp3Url, path=os.path.join(str(_id), str(item['id'])))
                 lyric = api.get_song_lyric(ids=song_id)
-                mongo.save_music_info(song_info=song, album_name=item['name'], artist=artist, mp3Url=path, lyric=lyric)
-        # 更新歌手专辑数量
-        mongo.update_album_count(artist=artist, album_count=count)
-        print('---------- {} albums saved ----------'.format(count))
+                mongo.save_song_info(song_info=song, album_id=item['id'], mp3Url=mp3Url, lyric=lyric)
+        print('---------- {} albums saved ----------'.format(album_count))
--- code/mongo.py
+++ code/mongo.py
@@ -1,29 +1,39 @@
 from pymongo import MongoClient
 from code import Commons
 
-
 class Mongo:
     def __init__(self):
         self.host = Commons.MONGO['host']
         self.port = Commons.MONGO['port']
         self.client = MongoClient(host=self.host, port=self.port)
         self.db = self.client.music
-        self.collections = self.db.album
-
-    # 保存歌手相关信息
+        self.collections = self.db.Artist
+    
+    # 保存歌手相关信息（Artist 集合）
     def save_artist_info(self, artist_info):
-        # 处理歌手信息
+        # 处理歌手数据
         data = {
             "name": artist_info['name'],
             "_id": artist_info['id'],
             "picUrl": artist_info['picUrl']
         }
-        # 新增该歌手的信息
-        result_id = self.collections.insert({"artist": data})
+        self.collections = self.db.Artist
+        # 开始保存
+        result_id = self.collections.insert(data)
         return result_id
 
-    # 保存单张专辑信息
-    def save_album_info(self, album_info, artist, pic_url):
+    # 根据歌手 id 查找 Album 中是否存在该条记录
+    def check_artist_in_album(self, artist_id):
+        self.collections = self.db.Album
+        result = self.collections.find_one({"artist_id": artist_id})
+        if result == None:
+            self.collections.insert_one({"artist_id": artist_id})
+            print("insert into artist: {} info".format(artist_id))
+        return
+    
+    # 保存单张专辑信息（根据歌手 id 存储）
+    def save_album_info(self, album_info, artist_id, pic_url):
+        self.check_artist_in_album(artist_id=artist_id)
         data = {
             "name": album_info['name'],
             "_id": album_info['id'],
@@ -35,26 +45,27 @@ class Mongo:
             "tags": album_info['tags'],
             "company": album_info['company']
         }
-        self.collections.update({"artist.name": artist}, {"$push": {"albums": data}})
+        self.collections = self.db.Album
+        self.collections.update({"artist_id": artist_id}, {"$push": {"albums": data}})
+
+    # 根据专辑 id 查找 Song 中是否存在该条记录
+    def check_album_in_song(self, album_id):
+        self.collections = self.db.Song
+        result = self.collections.find_one({"album_id": album_id})
+        if result == None:
+            self.collections.insert_one({"album_id": album_id})
+            print("insert into album: {} info".format(album_id))
         return
-
-    # 判断专辑是否存在
-    def find_if_exist(self, artist, album):
-        result = self.collections.find({"artist.name": artist, "albums.name": album})
-        return result.count()
-
+    
     # 保存歌曲信息
-    def save_music_info(self, song_info, album_name, artist, mp3Url, lyric):
+    def save_song_info(self, song_info, album_id, mp3Url, lyric):
+        self.check_album_in_song(album_id=album_id)
         data = {
             "name": song_info['name'],
             "_id": song_info['id'],
             "mp3Url": mp3Url,
             "lyric": lyric
         }
-        self.collections.update({"artist.name": artist, "albums.name": album_name}, {"$push": {"albums.$.songs": data}})
-        return
-
-    # 插入专辑数量字段
-    def update_album_count(self, artist, album_count):
-        self.collections.update({"artist.name": artist}, {"$set": {"albumCount": album_count}})
+        self.collections = self.db.Song
+        self.collections.update({"album_id": album_id}, {"$push": {"songs": data}})
         return
